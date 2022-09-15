6e3fe4fd412c66b3aae2d62c9e7985b38b3cae7e
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-09-15 19:27:47 +0900

    버튼 작동

--- app/src/main/java/com/example/breakfastforce/Fragment2.java
+++ app/src/main/java/com/example/breakfastforce/Fragment2.java
@@ -5,20 +5,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class Fragment2 extends Fragment {
-
+    ImageButton btn_sandwich;
 
     public Fragment2() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         // Inflate the layout for this fragment
+
+        View v = inflater.inflate(R.layout.activity_fragment2, container, false);
+        btn_sandwich = (ImageButton) v.findViewById(R.id.btn_sandwich);
+        btn_sandwich.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "샌드위치 클릭", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return inflater.inflate(R.layout.activity_fragment2, container, false);
     }
 
