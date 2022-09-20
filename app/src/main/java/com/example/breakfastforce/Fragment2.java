3d7c3ac0d501321eb147ac32cca7fcccc99ab3b5
olsi10 <wangbampang2035@gmail.com>
2022-09-20 20:08:44 +0900

    레시피 페이지 화면 전환 시도 (새파일 몇 개 있음)

--- app/src/main/java/com/example/breakfastforce/Fragment2.java
+++ app/src/main/java/com/example/breakfastforce/Fragment2.java
@@ -1,7 +1,14 @@
 package com.example.breakfastforce;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,8 +17,26 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 
+class fragment2Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton test = (ImageButton) findViewById(R.id.test);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
 public class Fragment2 extends Fragment {
-    ImageButton btn_sandwich;
+//    ImageButton btn_sandwich;
 
     public Fragment2() {
         // Required empty public constructor
@@ -23,16 +48,28 @@ public class Fragment2 extends Fragment {
         super.onCreate(savedInstanceState);
         // Inflate the layout for this fragment
 
-        View v = inflater.inflate(R.layout.activity_fragment2, container, false);
-        btn_sandwich = (ImageButton) v.findViewById(R.id.btn_sandwich);
-        btn_sandwich.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "샌드위치 클릭", Toast.LENGTH_SHORT).show();
-            }
-        });
+
+
+//        View v = inflater.inflate(R.layout.activity_fragment2, container, false);
+//        btn_sandwich = (ImageButton) v.findViewById(R.id.btn_sandwich);
+//        btn_sandwich.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getActivity(), "샌드위치 클릭", Toast.LENGTH_SHORT).show();
+//                Intent in = new Intent();
+//                startActivity(in);
+//            }
+//        });
 
         return inflater.inflate(R.layout.activity_fragment2, container, false);
     }
 
+}
+
+class SubActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment2_detail);
+    }
 }
