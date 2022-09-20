6897546341be7ec591e371ec6d30e6128ee4fdd1
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-09-20 18:52:25 +0900

    플로팅버튼 추가

--- app/src/main/java/com/example/breakfastforce/Fragment2.java
+++ app/src/main/java/com/example/breakfastforce/Fragment2.java
@@ -2,17 +2,20 @@ package com.example.breakfastforce;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 
 public class Fragment2 extends Fragment {
+    FrameLayout frame_sandwich;
     ImageButton btn_sandwich;
-
     public Fragment2() {
         // Required empty public constructor
     }
@@ -22,13 +25,19 @@ public class Fragment2 extends Fragment {
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Inflate the layout for this fragment
-
         View v = inflater.inflate(R.layout.activity_fragment2, container, false);
-        btn_sandwich = (ImageButton) v.findViewById(R.id.btn_sandwich);
+        frame_sandwich = v.findViewById(R.id.frame_sandwich);
+        btn_sandwich = v.findViewById(R.id.btn_sandwich);
+        frame_sandwich.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "하이", Toast.LENGTH_SHORT).show();
+            }
+        });
         btn_sandwich.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), "샌드위치 클릭", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "하이", Toast.LENGTH_SHORT).show();
             }
         });
 
