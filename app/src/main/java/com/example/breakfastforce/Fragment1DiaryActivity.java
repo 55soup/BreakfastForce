b4caa1edd26317478191677d6e90467581b0d5cd
olsi10 <wangbampang2035@gmail.com>
2022-10-13 20:02:14 +0900

    뒤로가기 버튼 완성 / 일기 xml 완성

--- app/src/main/java/com/example/breakfastforce/Fragment1DiaryActivity.java
+++ app/src/main/java/com/example/breakfastforce/Fragment1DiaryActivity.java
@@ -15,12 +15,20 @@ import android.widget.ImageButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Fragment1DiaryActivity extends AppCompatActivity {
+    ImageButton btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment1_diary);
 
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed(); // 뒤로 가기 버튼
+            }
+        });
 
     }
 }
