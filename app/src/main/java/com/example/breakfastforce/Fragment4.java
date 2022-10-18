96ead2fd3e09d98172d71674d1cfe245b929bc39
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-10-19 00:32:06 +0900

    fragment4 리스트뷰 구현

--- app/src/main/java/com/example/breakfastforce/Fragment4.java
+++ app/src/main/java/com/example/breakfastforce/Fragment4.java
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 
 public class Fragment4 extends Fragment {
     FloatingActionButton fab_btn;
+    ListView list;
+    ListItemAdapter adapter;
+
     public Fragment4() {
         // Required empty public constructor
     }
@@ -22,13 +28,27 @@ public class Fragment4 extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.activity_fragment4, container, false);
         fab_btn = v.findViewById(R.id.fab_btn);
+        list = (ListView) v.findViewById(R.id.list);
+        adapter = new ListItemAdapter();
         fab_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
-        return inflater.inflate(R.layout.activity_fragment4, container, false);
+
+        adapter.addItem(new ListItem("아침을 먹어야하는 이유", "2020년 부터 매일 아침을 먹기로\n" +
+                "마음 먹었다. 그 뒤로 꾸준히 가리지않고\n" +
+                "일정한 시간 밥을 먹었다.", "2022.10.19"));
+        adapter.addItem(new ListItem("아침을 먹어야하는 이유", "2020년 부터 매일 아침을 먹기로\n" +
+                "마음 먹었다. 그 뒤로 꾸준히 가리지않고\n" +
+                "일정한 시간 밥을 먹었다.", "2022.10.19"));
+        adapter.addItem(new ListItem("아침을 먹어야하는 이유", "2020년 부터 매일 아침을 먹기로\n" +
+                "마음 먹었다. 그 뒤로 꾸준히 가리지않고\n" +
+                "일정한 시간 밥을 먹었다.", "2022.10.19"));
+        list.setAdapter(adapter);
+
+        return v;
 
     }
 }
